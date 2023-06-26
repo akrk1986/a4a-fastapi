937981f79b56d53e3eef11198bcc824d81af1c65
amir <akrk1986@gmail.com>
2023-06-26 19:23:44 +0300

    Works, before adding unit tests

--- app.py
+++ app.py
@@ -1,49 +1,18 @@
-# app.py
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
+"""app.py - main code of A4A excercise"""
+from fastapi import FastAPI
 # project files
 from players import Player
 
 
 app = FastAPI()
-
-players_loaded = False
-
-
-def _find_next_id():
-    return max(country.country_id for country in countries) + 1
-
-
-class Country(BaseModel):
-    country_id: int = Field(default_factory=_find_next_id, alias="id")
-    name: str
-    capital: str
-    area: int
-
-
-countries = [
-    Country(id=1, name="Thailand", capital="Bangkok", area=513120),
-    Country(id=2, name="Australia", capital="Canberra", area=7617930),
-    Country(id=3, name="Egypt", capital="Cairo", area=1010408),
-]
-
-countries_dict = {}
-for country in countries:
-    countries_dict[country.country_id] = country.name
+player_obj = Player(csv_file='Data/Player.csv', debug=True)
 
 
 @app.get("/api/players")
-async def get_players():
-    fu = Player(csv_file='players.csv', debug=True)
-    return countries
+def get_players():
+    return player_obj.return_all()
 
 
 @app.get("/api/players/{player_id}")
-async def get_player(player_id: int):
-    if not players_loaded:
-        fu = Player(csv_file='players.csv', debug=True)
-    if player_id in countries_dict:
-        return countries_dict[player_id]
-    raise HTTPException(status_code=404,
-                        detail=f"Error: no player with ID '{player_id}'")
-
+def get_player(player_id: str):
+    return player_obj.return_one(player_id=player_id)
--- players.py
+++ players.py
@@ -1,4 +1,4 @@
-"""Handle the players data."""
+"""Handle the Player.csv data."""
 
 import csv
 from pathlib import Path
@@ -9,10 +9,31 @@ class Player:
     def __init__(self, csv_file: str, debug: bool):
         self.csv_file = csv_file
         self.debug = debug
-
+        self.rows = []          # list of dicts
+        self.rows_dict = {}
+        print(f"CSV='{csv_file}'")
         csv_path = Path(self.csv_file)
         if not csv_path.is_file():
-            return
-            raise HTTPException(status_code=500,
-                                detail=f"Error: Players file '{self.csv_file}' is not accessible or is not a file",
-                                headers={"X-Error": "Players CSV file is not found at expected location"})
+            raise HTTPException(
+                status_code=500,
+                detail=f"Error: Players file '{self.csv_file}' is not accessible or is not a file")
+
+        with csv_path.open(mode='r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.rows.append(row)
+                player_id = row['playerID']
+                if player_id in self.rows_dict:
+                    raise HTTPException(
+                        status_code=500,
+                        detail=f"Error: Players file '{self.csv_file}' has a duplicate Player ID '{player_id}'")
+                self.rows_dict[player_id] = row
+
+    def return_all(self):
+        return self.rows
+
+    def return_one(self, player_id: str):
+        if player_id in self.rows_dict:
+            return self.rows_dict[player_id]
+        raise HTTPException(status_code=404,
+                            detail=f"Error: Player ID '{player_id}' is not a valid Player ID")
