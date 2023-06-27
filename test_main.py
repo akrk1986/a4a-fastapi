b08458e9537d524b524ba174fab1c952d639cf55
amir <akrk1986@gmail.com>
2023-06-27 15:31:05 +0300

    Added unit tests

---
+++ test_main.py
@@ -0,0 +1,22 @@
+"""Testing code for A4A REST API calls"""
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+
+def test_get_all():
+    response = client.get("/api/players")
+    assert response.status_code == 200
+
+
+def test_get_nonexistent_item():
+    response = client.get("/api/players/fubar")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Error: Player ID 'fubar' is not found in the player CSV file"}
+
+
+def test_nonexistent_path():
+    response = client.get("/api/fubar")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
